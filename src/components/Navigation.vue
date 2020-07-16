0d8e156e06bdf92ea22402ea703ff390362d370a
premraj59128 <premraj59128@protonmail.com>
2020-07-16 11:46:55 +0530

    Added Basic Nav elements

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,33 +1,18 @@
 <template>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h1 class="hidden-sm-and-down">Migraine Hospital</h1>
+        <h1 class="hidden-md-and-up title-text">Migraine Hospital</h1>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="#"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Social Media</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
@@ -40,5 +25,7 @@ export default {
 </script>
 
 <style scoped>
-
+.title-text {
+    font-size: 1em;
+}
 </style>
